068e091de10fade3dbcc1ee048bbc26974c45bf1
Ishika Saxena <saxena.ishi05@gmail.com>
2024-04-01 00:43:14 +0530

    created common.css

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Function to handle booking a ride
 function bookRide() {
-    window.location.href = "booking.html"; 
-    // You can add your booking logic here
-    alert("Your ride has been booked!"); // For demonstration purposes, showing an alert
+    window.location.href = "booking.html"; // Redirect to the booking page
 }
